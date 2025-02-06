36fd87239331b58cc1ddedab3a0e62f83e5dbbf9
jahangeer7704 <jahangeer7704@gmail.com>
2025-02-06 16:09:32 +0530

    brandsComponent

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Test from "./pages/Test";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TeamIcons from '../components/common/Brands'
+const Test = () => {
+  return (
+    <div className='bg-black h-[100vh] flex  items-center justify-center '>
+        <TeamIcons />
+    </div>
+  )
+}
+
+export default Test
