81caf8b8bf8981253eb22253f132de2863ac7ee5
gowdaman-dev <damangowdaman@gmail.com>
2025-02-06 17:49:13 +0530

    smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Navbar from "./components/common/Navbar";
+import LenisScroll from "./components/common/LenisScroll";
 function App() {
   return (
     <div className="">
@@ -14,14 +15,16 @@ function App() {
         </div>
       </div>
       <div className="relative z-0 w-full">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </BrowserRouter>
+        <LenisScroll>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </BrowserRouter>
+        </LenisScroll>
       </div>
     </div>
   );
