0e68e2c321c648d7ac7b01ed99fd79ff0ae5a7ad
deepath15 <deepath1506@gmail.com>
2025-02-06 20:34:45 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
             <Navbar />
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
+              <Route path="about" element={<About />} />
+              <Route path="contact" element={<Contact />} />
             </Routes>
           </BrowserRouter>
         </LenisScroll>
