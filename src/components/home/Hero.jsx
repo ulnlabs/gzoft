87d0d6add4e479fb27f8f1a5b579ed6cf316fede
gowdaman-dev <damangowdaman@gmail.com>
2025-02-06 18:29:11 +0530

    patch

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -10,10 +10,10 @@ function Hero() {
             <p className="text-black -rotate-3">Digital</p>
           </span>
         </h1>
-        <h1 className="flex gap-2 text-6xl max-md:text-5xl max-sm:text-3xl">
+        <h1 className="flex gap-2 text-6xl max-md:text-5xl max-sm:text-3xl h-fit">
           <p>Marketing</p>
           <div className="bg-primary rounded-full">
-            <img src="/images/hero-mic.avif" className="min-h-15 min-w-15 max-sm:max-h-5 max-sm:max-w-5 max-w-15 max-h-15" alt="" />
+            <img src="/images/hero-mic.avif" className="w-[4rem]" alt="" />
           </div>
           <p>Agency.</p>
         </h1>
