075bffd00483328b602c68952ebfe14f5ed25abd
gowdaman-dev <damangowdaman@gmail.com>
2025-02-08 07:15:31 +0530

    a

--- src/components/common/Stats.jsx
+++ src/components/common/Stats.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Stats({ num, sub }) {
   return (
-    <div className="w-[150px] h-[150px] rounded-full bg-white/10 flex items-center justify-center items-center flex-col">
+    <div className="w-[150px] h-[150px] rounded-full bg-white/10 flex items-center justify-center flex-col">
       <h1 className="text-4xl text-white">{num}</h1>
-      <p className="text-white text-xs font-light text-zinc-300">{sub}</p>
+      <p className="text-white text-xs font-light">{sub}</p> 
     </div>
   );
 }
