43a5a110482d85befbb79dd39e44434b03a88c69
gowdaman-dev <damangowdaman@gmail.com>
2025-02-08 17:17:06 +0530

    new

--- src/components/common/LenisScroll.jsx
+++ src/components/common/LenisScroll.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { ReactLenis } from "lenis/react";
 function LenisScroll({ children }) {
   return (
-    <div className="max-sm:hidden">
-      <ReactLenis
-        options={{
-          duration: 2,
-        }}
-        root
-      >
-        {children}
-      </ReactLenis>
+    <>
+      <div className="max-sm:hidden">
+        <ReactLenis
+          options={{
+            duration: 2,
+          }}
+          root
+        >
+          {children}
+        </ReactLenis>
+      </div>
       <div className="sm:hidden">{children}</div>
-    </div>
+    </>
   );
 }
 
