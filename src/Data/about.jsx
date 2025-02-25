255b46bc88076dfe49120d0df8bb167e972bf964
gowdaman-dev <damangowdaman@gmail.com>
2025-02-25 20:06:48 +0530

    new

--- src/Data/about.jsx
+++ src/Data/about.jsx
@@ -1,92 +1,99 @@
-
 export const teamMembers = [
-    {
-      title: "Visionary",
-      title2: "Innovation Pioneer",
-      name: "Sylvia Charley",
-      position: "Founder",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Enterprise ",
-      title2: "Development Catalyst",
-      name: "Dr S Sumathi",
-      position: "Director",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Innovative",
-      title2: "Technology Architect",
-      name: "Balaji Srinivassan",
-      position: "Tech Lead",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Precision",
-      title2: "Software Alchemist",
-      name: "Jahangeer I",
-      position: "Tech Associate",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Strategic",
-      title2: "Process Orchestrator",
-      name: "Charley Benjamin",
-      position: "Operations Head",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Training",
-      title2: "Excellence Advocate",
-      name: "Sushanth singh",
-      position: "Training Head",
-      image: "images/team/Sushant_Singh.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-    {
-      title: "Customer",
-      title2: "Experience Orchestrator",
-      name: "Parvathi",
-      position: "Client Relations Manager",
-      image: "images/team/Sylvia.png",
-      links: ["https://github.com", "#", "#", "#"]
-    },
-  ]
-  
-  export const highLights = [
-    { text: "Innovative Strategist" },
-    { text: "Tech Visionary" },
-    { text: "Transformational Leader" },
-    { text: "Knowledge Architect" },
-  ]
-  export const workSteps = [
-    {
-      number: "01",
-      stage: "Discovery Phase",
-      description: "We identify your target audience, understand their needs, and create a compelling content strategy."
-  
-    },
-    {
-      number: "02",
-      stage: "Development Phase",
-      description: "We build a strong content team, implement relevant SEO strategies, and optimize your website for search engines."
-    },
-    {
-      number: "03",
-      stage: "Launch Phase",
-      description: "We launch your website, engage with your audience, and monitor your performance to continually improve."
-    },
-    {
-      number: "04",
-      stage: "Monitoring Phase",
-      description: "We launch your website, engage with your audience, and monitor your performance to continually improve."
-    },
-  
-  
-  ]
-  
+  {
+    title: "Visionary",
+    title2: "Innovation Pioneer",
+    name: "Sylvia Charley",
+    position: "Founder",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Enterprise ",
+    title2: "Development Catalyst",
+    name: "Dr S Sumathi",
+    position: "Director",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Innovative",
+    title2: "Technology Architect",
+    name: "Balaji Srinivassan",
+    position: "Tech Lead",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Precision",
+    title2: "Software Alchemist",
+    name: "Jahangeer I",
+    position: "Tech Associate",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Strategic",
+    title2: "Process Orchestrator",
+    name: "Charley Benjamin",
+    position: "Operations Head",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Training",
+    title2: "Excellence Advocate",
+    name: "Sushanth singh",
+    position: "Training Head",
+    image: "images/team/Sushant_Singh.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Client Relations",
+    title2: "Manager",
+    name: "Saraswati Yadav",
+    position: "Client Relations Manager",
+    image: "images/team/Saraswathi Yadav.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+  {
+    title: "Customer",
+    title2: "Experience Orchestrator",
+    name: "Parvathi",
+    position: "Client Relations Manager",
+    image: "images/team/Sylvia.png",
+    links: ["https://github.com", "#", "#", "#"],
+  },
+];
+
+export const highLights = [
+  { text: "Innovative Strategist" },
+  { text: "Tech Visionary" },
+  { text: "Transformational Leader" },
+  { text: "Knowledge Architect" },
+];
+export const workSteps = [
+  {
+    number: "01",
+    stage: "Discovery Phase",
+    description:
+      "We identify your target audience, understand their needs, and create a compelling content strategy.",
+  },
+  {
+    number: "02",
+    stage: "Development Phase",
+    description:
+      "We build a strong content team, implement relevant SEO strategies, and optimize your website for search engines.",
+  },
+  {
+    number: "03",
+    stage: "Launch Phase",
+    description:
+      "We launch your website, engage with your audience, and monitor your performance to continually improve.",
+  },
+  {
+    number: "04",
+    stage: "Monitoring Phase",
+    description:
+      "We launch your website, engage with your audience, and monitor your performance to continually improve.",
+  },
+];
